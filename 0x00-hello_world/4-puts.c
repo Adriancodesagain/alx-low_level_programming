d2bac0777f66a6a9d3e1bb8644f6fc9085ea5469
adriancodesagain <mwangi.k.adrian@gmail.com>
2023-02-16 18:32:18 +0300

    a c program rinting certain words

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main-program starts here,the program prints Programming is like building a multilingual puzzle using the puts function.
+ * Return:the return is 0 since the file runs smoothlythus a success.
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+return (0); }
