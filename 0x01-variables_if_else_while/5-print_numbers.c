0f280084c0109c63be7f68c197131731b2c1aad5
adriancodesagain <mwangi.k.adrian@gmail.com>
2023-02-19 13:23:54 +0300

    printing numbers in binary from 0 to 9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-/*
- *
- *
- */
+/**
+  * main - Entry point
+  *
+  * description: 'program prints out numbers in base 10.'
+  *
+  * Return: always 0 (success)
+  */
+
 int main(void)
 {
 	int i = 0;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		printf("%d" , i);
+		printf("%d", i);
 
 	}
 		printf("\n");
